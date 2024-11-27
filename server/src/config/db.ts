1beb02ced5041023b8f65eeca71fd2d47e7cc111
sonvh <sonvh@bravebits.vn>
2024-11-27 13:47:52 +0700

    debug env

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -5,6 +5,9 @@ import mongoose from "mongoose";
 async function connectMongoDB(): Promise<void> {
   try {
     const uri = process.env.MONGO_URI || "mongodb://localhost:27017/jwt"
+    console.log("🚀 ~ connectMongoDB ~ uri:", uri)
+    console.log("🚀 ~ connectMongoDB ~ ENV:", process.env.NODE_ENV)
+    console.log("🚀 ~ connectMongoDB ~ CLIENT_BUILD_PATH:", process.env.CLIENT_BUILD_PATH)
     if (!uri) {
       console.error("MongoDB URI does not exist!")
       process.exit(1)
